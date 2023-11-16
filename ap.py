a6e59eb3bd4b9bd2c2942836ec1b6a0dffe003ef
Qunoot15 <151042309+Qunoot15@users.noreply.github.com>
2023-11-16 18:57:13 +0500

    Create ap.py

---
+++ ap.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import spacy
+
+# Download spaCy model
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    st.warning("Downloading spaCy model. This may take some time.")
+    spacy.cli.download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+# Rest of your Streamlit app code
+
+# Streamlit app
+def main():
+    st.title("Named Entity Recognition (NER) Demo")
+
+    # User input
+    text_input = st.text_area("Enter text:", "John Doe is the CEO of ABC Corp, and it is located in New York.")
+
+    # NER processing
+    if st.button("Extract Entities"):
+        doc = nlp(text_input)
+
+        # Display entities
+        entities = [(ent.text, ent.label_) for ent in doc.ents]
+        st.write("Named Entities:")
+        for entity, label in entities:
+            st.write(f"- {entity} ({label})")
+
+if _name_ == "_main_":
+    main()
